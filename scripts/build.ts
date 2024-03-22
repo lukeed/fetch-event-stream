55834f5f9f038584a63fcd6f4614a7352c1a669d
Luke Edwards <luke.edwards05@gmail.com>
2024-03-22 13:26:46 -0700

    chore: top-level src

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,7 +3,7 @@ import { build, emptyDir } from 'https://deno.land/x/dnt@0.40.0/mod.ts';
 await emptyDir('./npm');
 
 await build({
-	entryPoints: ['./src/mod.ts'],
+	entryPoints: ['./mod.ts'],
 	outDir: './npm',
 	shims: {
 		// see JS docs for overview and more options
