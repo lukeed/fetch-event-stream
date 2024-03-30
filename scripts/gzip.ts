015095282f895053cc9c7216976b7aecff6e5011
Luke Edwards <luke.edwards05@gmail.com>
2024-03-29 22:44:31 -0700

    chore: update "gzip" task;
    - now uses `esbuild` directly

--- scripts/gzip.ts
+++ scripts/gzip.ts
@@ -1,47 +1,48 @@
-// deno-lint-ignore-file no-deprecated-deno-api
+import { build } from 'npm:esbuild@0.20.2';
+import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader@^0.10.3';
 
-import { assert } from '@std/assert';
+function toAbsolute(id: string) {
+	return new URL(import.meta.resolve(id)).pathname;
+}
 
-let bundle = await Deno.run({
-	cmd: [Deno.execPath(), 'bundle', 'mod.ts'],
-	stdout: 'piped',
-}).output();
-
-let minify = Deno.run({
-	cmd: ['minify', '--type=js'],
-	stdin: 'piped',
-	stdout: 'piped',
+let result = await build({
+	bundle: true,
+	format: 'esm',
+	entryPoints: [
+		'./mod.ts',
+	],
+	minify: true,
+	treeShaking: true,
+	write: false,
+	plugins: denoPlugins({
+		configPath: toAbsolute('../deno.json'),
+		lockPath: toAbsolute('../deno.lock'),
+	}),
 });
 
+let output = result.outputFiles[0].contents;
+
 let bytes = {
-	min: 0,
+	min: output.length,
 	gzip: 0,
 };
 
-let stream = minify.stdout.readable.pipeThrough(
-	new TransformStream({
-		transform(chunk, ctrl) {
-			bytes.min += chunk.length;
-			ctrl.enqueue(chunk);
-		},
-	}),
-).pipeThrough(
+let stream = new ReadableStream({
+	start(ctrl) {
+		ctrl.enqueue(output);
+		ctrl.close();
+	},
+}).pipeThrough(
 	new CompressionStream('gzip'),
 );
 
-await minify.stdin.write(bundle);
-minify.stdin.close();
-
-let pid = await minify.status();
-assert(pid.code === 0, 'minify error');
-
 let reader = stream.getReader();
+
 while (true) {
 	let tmp = await reader.read();
 	if (tmp.done) break;
 	bytes.gzip += tmp.value.length;
 }
 
-console.log('bytes (raw):', bundle.length.toLocaleString());
 console.log('bytes (minify):', bytes.min.toLocaleString());
 console.log('bytes (gzip):', bytes.gzip.toLocaleString());
